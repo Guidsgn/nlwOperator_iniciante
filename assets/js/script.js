eebc023227dc3749953cd9bd15a2b6acb71b7cf3
Guilherme.dsgn <guidesignvendas@gmail.com>
2026-03-13 13:30:33 -0300

    feat: add success toast notification for image processing

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,16 @@ const configMyWidget = {
 const myWidget = cloudinary.createUploadWidget(configMyWidget, (error, result) => {
     if (!error && result && result.event === "success") {
         console.log('Pronto! Aqui estão as informações da imagem: ', result.info);
+
+        Toastify({
+            text: "Vídeo processado com sucesso!",
+            duration: 3000,
+            gravity: "top",
+            position: "right",
+            style: {
+              background: "linear-gradient(to right, #00b09b, #96c93d)"
+            }
+          }).showToast();
     }
 }
 )
